7d1f323a4c26bfcd421415435bbf29fa3860f758
ErickMejicanos <164571399+ErickMejicanos@users.noreply.github.com>
2024-05-06 12:38:53 -0600

    Add files via upload

---
+++ lazo-while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while (i<10)
+    {
+        cout << i << "\n" ; 
+        i++; //Sirve para ir aumentado //
+    }
+    return 0;
+}
